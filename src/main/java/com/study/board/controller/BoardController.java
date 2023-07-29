7e0d1717a8963bd45931f923d75d340168ba4ff3
DOOHEE.LEE <doodooworksweb@gmail.com>
2023-07-30 02:20:43 +0900

    Initial commit

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,88 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write") //글쓰기 페이지로 이동
+    public String boardWriteForm(){
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writedo") //글쓰기페이지에서 데이터 받아서 Insert 처리
+    public String boardWritePro(Board board){
+
+        System.out.println("제목 : " + board.getTitle());
+        System.out.println("내용 : "+ board.getContent());
+
+        boardService.boardWrite(board);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/list") //리스트페이지
+    public String boardList(Model model, @PageableDefault(page=0, size=10, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+
+        Page<Board> list = boardService.boardList(pageable);
+
+        //현재 패이지 기준으로 앞 4페이지 , 뒤 5페이지
+        int curPage = list.getPageable().getPageNumber() + 1;
+        int staPage = Math.max(curPage - 4 , 1);
+        int finPage = Math.min(curPage + 5, list.getTotalPages());
+
+        model.addAttribute("list", list);
+        model.addAttribute("curPage", curPage);
+        model.addAttribute("staPage", staPage);
+        model.addAttribute("finPage", finPage);
+
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view") //상세보기 페이지
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete") //삭제처리 후 list 페이지로 이동
+    public String boardDelete(Integer id){
+
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}") //수정페이지로 이동
+    public String boardModify(@PathVariable("id") Integer id, Model model){
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}") //수정페이지에서 데이터 받아서 UPDATE
+    public String boardUpdate(@PathVariable("id") Integer id, Board board){
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.boardWrite(boardTemp);
+
+        return "redirect:/board/list";
+    }
+}
